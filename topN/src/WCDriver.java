703031e24a0a884dc6e2e8d13d7cfdbcee09e23e
shreeharikulkarni907 <63466408+shreeharikulkarni907@users.noreply.github.com>
2022-07-15 06:24:35 +0530

    Add files via upload

---
+++ topN/src/WCDriver.java
@@ -0,0 +1,32 @@
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+public class WCDriver {
+public static void main(String[] args) throws Exception {
+Configuration conf = new Configuration();
+String[] otherArgs = (new GenericOptionsParser(conf,
+args)).getRemainingArgs();
+if (otherArgs.length != 2) {
+System.err.println("Usage: TopN <in> <out>");
+System.exit(2);
+}
+Job job = Job.getInstance(conf);
+job.setJobName("Top N");
+job.setJarByClass(WCDriver.class);
+job.setMapperClass(WCMapper.class);
+job.setReducerClass(WCReducer.class);
+job.setOutputKeyClass(Text.class);
+job.setOutputValueClass(IntWritable.class);
+FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+FileOutputFormat.setOutputPath(job, new
+Path(otherArgs[1]));
+System.exit(job.waitForCompletion(true) ? 0 : 1);
+}
+}
